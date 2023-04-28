131407ba9e1f0ad9842f0496b160e278f4e5b49f
zhongliang001 <coolzhongliang@126.com>
2023-04-28 22:02:37 +0800

    feat: 完成注册中心和网关

---
+++ gateway/src/main/java/com/zl/mall/gateway/Application.java
@@ -0,0 +1,13 @@
+package com.zl.mall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class);
+	}
+	
+}
