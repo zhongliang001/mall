a087f5625949dc6a3eccf9a81d30b1c992dda438
zhongliang001 <coolzhongliang@126.com>
2023-04-29 23:03:26 +0800

    feat:创建uaa项目

---
+++ uaa/src/main/java/com/zl/mall/uaa/Application.java
@@ -0,0 +1,14 @@
+package com.zl.mall.uaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class);
+	}
+}
