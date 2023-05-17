78a209e00b71e3778d48e6922cd8b1073fdde12b
zhongliang001 <coolzhongliang@126.com>
2023-05-17 19:10:07 +0800

    feat：完成登录

---
+++ gateway/src/main/java/com/zl/mall/gateway/config/RestTemplateConfig.java
@@ -0,0 +1,29 @@
+package com.zl.mall.gateway.config;
+
+import java.nio.charset.Charset;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+	@Bean
+    public RestTemplate restTemplate(ClientHttpRequestFactory factory) {
+        RestTemplate restTemplate = new RestTemplate(factory);
+        restTemplate.getMessageConverters()
+            .set(1, new StringHttpMessageConverter(Charset.forName("UTF-8")));
+        return restTemplate;
+    }
+
+    @Bean
+    public ClientHttpRequestFactory simpleClientHttpRequestFactory() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setReadTimeout(5000);
+        factory.setConnectTimeout(5000);
+        return factory;
+    }
+}
