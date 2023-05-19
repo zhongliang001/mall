1691d433ab1e112f7a746a9f4019acc7e39a8352
zhongliang001 <coolzhongliang@126.com>
2023-05-19 17:49:45 +0800

    feat:新增主页

--- mall-web/src/router/index.ts
+++ mall-web/src/router/index.ts
@@ -1,9 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import Login from '@/pages/Login.vue'
+import Home from "@/pages/Home.vue"
+import Register from '@/pages/Register.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    
+    {
+      name: 'login',
+      path: '/login',
+      component: Login
+    },
+    {
+      name: 'home',
+      path: '/home',
+      component: Home,
+      children:[{
+        name: 'register',
+        path: '/register',
+        component: Register
+      }]
+    },
   ]
 })
 
