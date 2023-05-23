60cd20f9195886d6076b936b456279df1291d4c1
zhongliang001 <coolzhongliang@126.com>
2023-05-23 16:42:40 +0800

    feat: 用户退出功能实现

---
+++ mall-user/mall-user-dto/src/main/java/com/zl/mall/user/userauth/dto/UserAuthLogOutDto.java
@@ -0,0 +1,33 @@
+package com.zl.mall.user.userauth.dto;
+
+import java.io.Serializable;
+
+public class UserAuthLogOutDto implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private String userId;
+	
+	private String token;
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+	
+	
+}
